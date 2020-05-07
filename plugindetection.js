adf77bda792167b4b3c76a0bebb136db50beacc4
YeahSpang <github@arcticiced.com>
2020-05-07 20:15:50 +0200

    Added plugin detection for ItemHerald.com, added itemherald permissions to manifest

---
+++ plugindetection.js
@@ -0,0 +1,4 @@
+//Add element to itemherald website to show that the plugin is installed
+var element = document.createElement("div");
+element.classList.add("plugin-installed");
+document.body.append(element);
